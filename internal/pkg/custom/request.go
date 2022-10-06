e4c7d89f109dd5909f94e5169488d87c936a827e
JuditSatriaN <judit.nugroho@tokopedia.com>
2022-10-06 09:22:33 +0700

    test undo commit 3

--- internal/pkg/custom/request.go
+++ internal/pkg/custom/request.go
@@ -42,7 +42,7 @@ func BuildPageAndLimit(ctx *fiber.Ctx) (int, int, error) {
 	return page, limit, nil
 }
 
-// BuildOffset global function to build offset
+// BuildOffset global function to build offset asdad
 func BuildOffset(page int, limit int) int {
 	offset := (page - 1) * limit
 	return offset
